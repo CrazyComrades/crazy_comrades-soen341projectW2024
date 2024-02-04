38ad391be5d2123d5b7fc6aa9b677352934014b5
Mohamed Mrizek <rayan.mrizek@gmail.com>
2024-02-04 18:55:35 -0500

    linked the html file

--- Car_rental_app/website/views.py
+++ Car_rental_app/website/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1> Welcome to our rental car app"
+    return render_template("base.html")
     
