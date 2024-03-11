239f381b572a30a28a5687a5a47e0715f6dcfd01
Yazid Dawiss <104469015+whoisyazzy@users.noreply.github.com>
2024-03-10 23:45:54 -0400

    Implemented Logout and Initialized the models

--- Car_rental_app/website/models.py
+++ Car_rental_app/website/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy import func
 from . import db
 from flask_login import UserMixin
 
@@ -7,16 +8,29 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     role = db.Column(db.String(150))
+    reservations = db.relationship('Reservation')
     
 
 class Vehicle(db.Model): 
     id = db.Column(db.Integer, primary_key=True)
-
+    type_car = db.Column(db.String(150))
+    brand = db.Column(db.String(150))
+    car_model = db.Column(db.String(150))
+    year = db.Column(db.Integer)
+    price = db.Column(db.Double)
+    mileage = db.Column(db.Integer)
+    availability = db.Column (db.Boolean)
 
     
 class Reservation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    
+    checkin = db.Column(db.DateTime(timezone=True), default=func.now())
+    checkout = db.Column(db.DateTime(timezone=True), default=func.now())
+    final_price = db.Column(db.Double)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'))
+    vehicle_price = db.Column(db.Integer, db.ForeignKey('vehicle.price'))
+    vehicle_avail = db.Column(db.Integer, db.ForeignKey('vehicle.availability'))
 
 
 
