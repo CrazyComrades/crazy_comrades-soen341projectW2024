8b6ab9684a18492c3d893b75a5a90723a928d735
Mohamed Mrizek <rayan.mrizek@gmail.com>
2024-03-21 18:31:15 -0400

    branches

--- Car_rental_app/website/views.py
+++ Car_rental_app/website/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template,request
-from .models import Vehicle
+from flask import Blueprint, render_template,request, jsonify
+from .models import Vehicle,Branch
 from flask import send_from_directory
 
 views = Blueprint('views', __name__)
@@ -14,6 +14,31 @@ def reservation():
     user_id = request.args.get('user_id')
     return render_template("reservation.html",vehicle_id=vehicle_id,user_id = user_id)    
 
+@views.route('/branches')
+def branches():
+    branches = Branch.query.all()
+    return render_template('branches.html', branches=branches)
+
+@views.route('/branch/<int:branch_id>/cars')
+def branch_cars(branch_id):
+    branch = Branch.query.get_or_404(branch_id)
+    cars = Vehicle.query.filter_by(branch_id=branch_id, availability=True).all()
+    return render_template('branch_cars.html', branch=branch, cars=cars)
+
+
+
+@views.route('/search_cars', methods=['GET'])
+def search_cars():
+    branch_id = request.args.get('branch_id')
+    car_type = request.args.get('car_type')
+    # Other search criteria
+
+    # Query cars based on selected branch and search criteria
+    available_cars = Vehicle.query.filter_by(branch_id=branch_id, type_car=car_type).all()  # Example query
+
+    # Serialize car data and return as JSON response
+    car_data = [{'id': car.id, 'type_car': car.type_car, 'brand': car.brand} for car in available_cars]
+    return jsonify(car_data)
 
 @views.route('/admin')
 def admin():
